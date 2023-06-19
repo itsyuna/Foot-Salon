62b580b6b10a79f08ca6dadd2e06196704cf19a0
itsyuna <ingyouna@naver.com>
2023-06-19 23:58:05 +0900

    Add Login button

--- src/compoents/organisms/Header/Header.tsx
+++ src/compoents/organisms/Header/Header.tsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Button from "../../atoms/Button/Button";
+import Login from "../../pages/Login/Login";
+import { Link } from "react-router-dom";
 
 const HeaderWrapper = styled.header`
   width: 100%;
@@ -7,10 +10,26 @@ const HeaderWrapper = styled.header`
 
 const logoImg = `${process.env.PUBLIC_URL}/assets/logo/header-logo.svg`;
 
+const loginHandler = () => {
+  <Login />;
+};
+
 const Header = () => {
   return (
     <HeaderWrapper>
       <img src={logoImg} alt="Header logo" />
+      <Link to="login">
+        <Button
+          type="button"
+          onClick={loginHandler}
+          buttonText="LOGIN"
+          backgroundColor="#E1FFB1"
+          color="#f266ab"
+          float="right"
+          position="relative"
+          top="5rem"
+        />
+      </Link>
     </HeaderWrapper>
   );
 };
