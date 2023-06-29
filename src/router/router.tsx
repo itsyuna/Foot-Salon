9b2c60bae3c687836f0cd89faa45d18ec37a21cc
itsyuna <ingyouna@naver.com>
2023-06-29 23:43:30 +0900

    Add Router-login/signup

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,6 +6,8 @@ import LineUp from "../compoents/pages/LineUp";
 import Photos from "../compoents/pages/Photos";
 import Play from "../compoents/pages/Play";
 import Stats from "../compoents/pages/Stats";
+import Login from "../compoents/pages/Login/Login";
+import SignUp from "../compoents/pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +20,8 @@ const router = createBrowserRouter([
       { path: "/half-time", element: <HalfTime /> },
       { path: "stats", element: <Stats /> },
       { path: "photos", element: <Photos /> },
+      { path: "login", element: <Login /> },
+      { path: "login/sign-up", element: <SignUp /> },
     ],
   },
 ]);
