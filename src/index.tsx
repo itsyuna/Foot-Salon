f38fd2c0eec5f66189b253cd2121dc733edda1ec
itsyuna <ingyouna@naver.com>
2023-07-13 22:57:58 +0900

    Modify index

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 import GlobalStyle from "./styles/global-style";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <>
+  <Provider store={store}>
     <GlobalStyle />
     <RouterProvider router={router} />
-  </>
+  </Provider>
 );
