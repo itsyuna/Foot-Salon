62f5f9b63872f5296375b2aaf083c69386d51863
itsyuna <ingyouna@naver.com>
2023-07-15 23:55:52 +0900

    Feat: Post

---
+++ src/compoents/pages/Post/Post.tsx
@@ -0,0 +1,12 @@
+import { MainWrapper } from "../../../App";
+import ReadPost from "../../organisms/ReadPost/ReadPost";
+
+const Post = () => {
+  return (
+    <MainWrapper>
+      <ReadPost />
+    </MainWrapper>
+  );
+};
+
+export default Post;
