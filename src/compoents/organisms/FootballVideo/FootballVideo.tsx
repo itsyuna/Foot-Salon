62cdd17ffa3b9a6109f9a4c3ad378e143187eadc
itsyuna <ingyouna@naver.com>
2023-08-05 12:59:48 +0900

    Feat: LineUp

---
+++ src/compoents/organisms/FootballVideo/FootballVideo.tsx
@@ -0,0 +1,147 @@
+import axios, { AxiosError } from "axios";
+
+import { SyntheticEvent, useEffect, useState } from "react";
+import styled from "styled-components";
+import ErrorMessage from "../../molecules/Message/ErrorMessage/ErrorMessage";
+
+interface LeagueName {
+  name: string;
+}
+
+interface TableProps {
+  title: string;
+  embed: string;
+  url: string;
+  thumbnail: string;
+  date: string;
+  competition: LeagueName;
+}
+
+const VideoItemWrapper = styled.section`
+  margin-top: 30px;
+  text-align: center;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(30%, auto));
+  column-gap: 10px;
+  row-gap: 20px;
+`;
+
+const MatchTeams = styled.p`
+  font-weight: 600;
+  font-size: 1.2rem;
+  margin-bottom: 0;
+`;
+
+const MatchDate = styled.p`
+  font-weight: 600;
+  color: #394867;
+  margin: 0;
+`;
+
+const Img = styled.img`
+  width: 300px;
+  height: 200px;
+`;
+
+let korea = "KOREA";
+let epl = "ENGLAND";
+let laliga = "SPAIN";
+let serieA = "ITALY";
+
+const FootballVideo = ({ userInput }: { userInput: string }) => {
+  let [table, setTable] = useState<TableProps[]>([]);
+
+  let [error, setError] = useState(null);
+
+  let getAPI = async () => {
+    const options = {
+      method: "GET",
+      url: `${process.env.REACT_APP_VIDEO_URL}`,
+      headers: {
+        "X-RapidAPI-Key": `${process.env.REACT_APP_KEY}`,
+        "X-RapidAPI-Host": `${process.env.REACT_APP_VIDEO_HOST}`,
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setTable(response.data);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        let errorStatus = error.response?.data.details;
+        setError(errorStatus);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getAPI();
+  }, []);
+
+  let dateHandler = (gameDate: string) => {
+    const date = gameDate.substring(0, 10);
+    const hour = gameDate.substring(11, 16);
+    const fullDateHour = `${date} ${hour}`;
+
+    return fullDateHour;
+  };
+
+  let koreaDateHandler = (gameDate: string) => {
+    const newDate = new Date(gameDate);
+
+    const year = newDate.getFullYear();
+    const month = String(newDate.getMonth() + 1).padStart(2, "0");
+    const date = String(newDate.getDate()).padStart(2, "0");
+    const hour = String(newDate.getHours()).padStart(2, "0");
+    const minute = String(newDate.getMinutes()).padStart(2, "0");
+
+    const koreaDate = `${year}-${month}-${date} ${hour}:${minute}`;
+
+    return koreaDate;
+  };
+
+  const searchList: TableProps[] = [];
+  table.map((list) => {
+    if (
+      list.competition.name.includes(korea) ||
+      list.competition.name.includes(epl) ||
+      list.competition.name.includes(laliga) ||
+      list.competition.name.includes(serieA)
+    ) {
+      searchList.push(list);
+    }
+    return searchList;
+  });
+
+  const filteredList = searchList.filter((item) => {
+    return item.title.toUpperCase().includes(userInput.toUpperCase());
+  });
+
+  const imgHandler = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = `${process.env.PUBLIC_URL}/assets/img/football-default-img.svg`;
+  };
+
+  return error === null ? (
+    <VideoItemWrapper>
+      {filteredList.map((list, idx) => (
+        <div key={idx}>
+          <MatchTeams>{list.title}</MatchTeams>
+          <div>
+            <a href={list.url} target="_blank" rel="noreferrer">
+              <Img src={list.thumbnail} alt="thumbnail" onError={imgHandler} />
+            </a>
+          </div>
+          <MatchDate>
+            {list.competition.name.includes(korea)
+              ? koreaDateHandler(list.date)
+              : dateHandler(list.date)}
+          </MatchDate>
+        </div>
+      ))}
+    </VideoItemWrapper>
+  ) : (
+    <ErrorMessage category="VIDEO" error={error} />
+  );
+};
+
+export default FootballVideo;
--- src/compoents/pages/LineUp/LineUp.tsx
+++ src/compoents/pages/LineUp/LineUp.tsx
@@ -1,10 +1,58 @@
+import styled from "styled-components";
 import { MainWrapper } from "../../../App";
+import Card from "../../../ui/Card/Card";
 import FootballVideo from "../../organisms/FootballVideo";
+import Input from "../../atoms/Input/Input";
+import { useState } from "react";
+
+const Title = styled.h1`
+  margin: 20px;
+  text-align: center;
+`;
+
+const SearchWrapper = styled.div`
+  height: 4vh;
+  text-align: center;
+  margin: 10px auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const GuideText = styled.p`
+  font-weight: 500;
+  margin-top: 2.3rem;
+`;
 
 const LineUp = () => {
+  let [userInput, setUserInput] = useState<string>("");
+
+  let searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
+  };
+
   return (
     <MainWrapper>
-      <FootballVideo />
+      <Card>
+        <section>
+          <Title>All Football matches Line-up</Title>
+          <SearchWrapper>
+            <Input
+              type="text"
+              placeholder="대,소문자 구분 없이 팀을 입력해주세요 :)"
+              value={userInput}
+              onChange={searchHandler}
+              autoComplete="off"
+              width="15vw"
+              height="4vh"
+            />
+            <GuideText>
+              영상 클릭시 해당 링크로 이동합니다!😉 (*경기시간: 현지 시간 기준)
+            </GuideText>
+          </SearchWrapper>
+        </section>
+        <FootballVideo userInput={userInput} />
+      </Card>
     </MainWrapper>
   );
 };
