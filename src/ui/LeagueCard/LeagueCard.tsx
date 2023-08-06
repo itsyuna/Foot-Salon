b4b18d7c483ec3c22696e58ec6aacc96620694c6
itsyuna <ingyouna@naver.com>
2023-08-07 01:53:09 +0900

    Modify LeagueCard - style

---
+++ src/ui/LeagueCard/LeagueCard.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import React from "react";
+
+export const LeagueWrapper = styled.article`
+  width: 30vw;
+  height: 100%;
+  background-color: white;
+  overflow: auto;
+`;
+
+const LeagueTab = styled.ul`
+  width: 100%;
+  height: 3vh;
+  margin: 0;
+`;
+
+const TabList = styled.li<ActiveTab>`
+  width: 100%;
+  height: 3vh;
+  line-height: 3vh;
+  font-family: "Bebas Neue", sans-serif;
+  font-size: 1.2rem;
+  border: 2px solid #a2cdcd;
+  background-color: ${({ isActive }) => (isActive ? "#feff86" : "white")};
+  color: ${({ isActive }) => (isActive ? "#4942e4" : "#27374d")};
+  font-weight: ${({ isActive }) => (isActive ? "900" : "")};
+`;
+
+interface ActiveTab {
+  isActive: boolean;
+}
+
+interface LeagueListItems {
+  name: string;
+  leagueNo: number;
+  content: JSX.Element[];
+}
+
+interface LeagueProps {
+  children: React.ReactNode;
+  leagueList: LeagueListItems[];
+  selectTabHandler: (idx: number, leagueNo: number) => void;
+  currentTab: number;
+}
+
+const LeagueCard = ({
+  children,
+  leagueList,
+  selectTabHandler,
+  currentTab,
+}: LeagueProps) => {
+  const LeagueTabHandler = (idx: number, leagueNo: number) => {
+    selectTabHandler(idx, leagueNo);
+    console.log(leagueNo);
+  };
+
+  return (
+    <LeagueWrapper>
+      <LeagueTab>
+        {leagueList.map((item, idx) => (
+          <TabList
+            key={idx}
+            onClick={() => LeagueTabHandler(idx, item.leagueNo)}
+            isActive={currentTab === idx ? true : false}
+          >
+            {item.name}
+          </TabList>
+        ))}
+      </LeagueTab>
+      {children}
+    </LeagueWrapper>
+  );
+};
+
+export default LeagueCard;
