147578345b62b3571c37e9a5ddcee82cf1dfbc0e
itsyuna <ingyouna@naver.com>
2023-08-10 23:54:48 +0900

    Add Stats - emotionList

---
+++ src/utils/emotion.ts
@@ -0,0 +1,17 @@
+export const emotionList = [
+  {
+    emotionId: 1,
+    emotionImage: process.env.PUBLIC_URL + `/assets/img/emotion-1.svg`,
+    emotionDescription: "lost",
+  },
+  {
+    emotionId: 2,
+    emotionImage: process.env.PUBLIC_URL + `/assets/img/emotion-2.svg`,
+    emotionDescription: "draw",
+  },
+  {
+    emotionId: 3,
+    emotionImage: process.env.PUBLIC_URL + `/assets/img/emotion-3.svg`,
+    emotionDescription: "win",
+  },
+];
