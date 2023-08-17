373d55ec0bae014c849209ab14700742104b6aec
itsyuna <ingyouna@naver.com>
2023-08-18 04:37:40 +0900

    Modify style - NoPostMessage

--- src/compoents/molecules/NoPostMessage/NoPostMessage.tsx
+++ src/compoents/molecules/NoPostMessage/NoPostMessage.tsx
@@ -1,37 +1,33 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
-import BoardCard from "../../../ui/BoardCard";
+import Card from "../../../ui/Card";
 import Button from "../../atoms/Button";
 
 const ErrorMessage = styled.section`
+  font-family: "Do Hyeon", sans-serif;
   width: 20%;
   height: 20vh;
   margin: 15rem auto;
   text-align: center;
 `;
 
-const NoPostMessage = ({ category }: { category: string }) => {
+const NoPostMessage = () => {
   const navigate = useNavigate();
 
-  const errorTargetPostHandler = () => {
-    if (category.includes("play")) navigate("/play");
-    else navigate("/half-time");
-  };
-
   return (
-    <BoardCard>
+    <Card>
       <ErrorMessage>
         <span>해당하는 게시글이 없습니다 :( </span>
         <Button
           type="button"
           backgroundColor="#75C2F6"
-          onClick={errorTargetPostHandler}
+          onClick={() => navigate(-1)}
         >
           뒤로가기
         </Button>
       </ErrorMessage>
-    </BoardCard>
+    </Card>
   );
 };
 
