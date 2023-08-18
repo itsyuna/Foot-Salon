693680c41e783f3a9cd92712e1ca052446d1cfb2
itsyuna <ingyouna@naver.com>
2023-08-18 19:41:52 +0900

    Modify SignUp

--- src/compoents/pages/SignUp/SignUp.tsx
+++ src/compoents/pages/SignUp/SignUp.tsx
@@ -1,20 +1,25 @@
 import styled from "styled-components";
-import { MainWrapper } from "../../../App";
-import Card from "../../../ui/Card/Card";
-import Input from "../../atoms/Input/Input";
-import Button from "../../atoms/Button/Button";
-import { Controller, useForm } from "react-hook-form";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from "../../../firebase/auth/auth";
 import { useNavigate } from "react-router-dom";
+import { Controller, useForm } from "react-hook-form";
+import { userActions } from "../../../store/user";
+import { useAppDispatch } from "../../../store";
+import { auth } from "../../../firebase/config";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
-const SingUpWrapper = styled.div`
+import Card from "../../../ui/Card";
+import Input from "../../atoms/Input";
+import Button from "../../atoms/Button";
+
+const SingUpWrapper = styled.article`
   width: 50%;
-  height: 90%;
-  margin: 0 auto;
-  margin-top: 7rem;
+  height: 55vh;
+  margin: 3rem auto 0;
   font-family: "Orelega One", cursive;
   text-align: center;
+
+  > button {
+    margin-right: 40rem;
+  }
 `;
 
 const Title = styled.h1`
@@ -23,8 +28,20 @@ const Title = styled.h1`
 
 export const ErrorText = styled.p`
   color: red;
-  margin: 0;
+  margin: 0 auto;
+  font-family: "Do Hyeon", sans-serif;
   font-size: 0.8rem;
+`;
+
+const InputWrapper = styled.div`
+  font-family: "Do Hyeon", sans-serif;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  margin-top: 1.5rem;
+`;
+
+const Label = styled.label`
+  line-height: 4vh;
 `;
 
 export interface FormData {
@@ -36,6 +53,7 @@ export interface FormData {
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const {
     handleSubmit,
@@ -59,119 +77,149 @@ const SignUp = () => {
 
       await updateProfile(user, { displayName: data.nickname });
 
+      dispatch(
+        userActions.login({
+          userNickname: user.displayName,
+          userEmail: user.email,
+          userUid: user.uid,
+        })
+      );
+
       alert("회원가입 성공!");
       navigate("/");
-      // history.push('/');
     } catch (error: any) {
-      console.log(error.message);
-      alert("오류");
+      alert("오류가 발생했습니다 :(");
     }
   };
 
   return (
-    <MainWrapper>
-      <Card>
-        <SingUpWrapper>
-          <Title>Sign up</Title>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-              control={control}
-              defaultValue=""
-              name="nickname"
-              rules={{
-                required: "필수 입력사항입니다.",
-                pattern: {
-                  value: /^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$/,
-                  message: "특수문자 or 공백을 제거해주세요.",
-                },
-              }}
-              render={({ field }) => (
+    <Card>
+      <SingUpWrapper>
+        <Button
+          type="button"
+          onClick={() => navigate(-1)}
+          width="5rem"
+          backgroundColor="#FFD36E"
+          border="#FEB139"
+          margin="0"
+        >
+          뒤로가기
+        </Button>
+        <Title>Sign up</Title>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            control={control}
+            defaultValue=""
+            name="nickname"
+            rules={{
+              required: "필수 입력사항입니다.",
+              pattern: {
+                value: /^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$/,
+                message: "특수문자 or 공백을 제거해주세요.",
+              },
+            }}
+            render={({ field }) => (
+              <InputWrapper>
+                <Label>닉네임</Label>
                 <Input
-                  label="닉네임"
                   type="text"
                   placeholder="한글,영문 사용 가능 / 띄어쓰기,특수문자 사용 불가"
                   value={field.value}
                   onChange={field.onChange}
                   autoComplete="off"
+                  width="20vw"
+                  height="4vh"
                 />
-              )}
-            />
-            <ErrorText>{errors.nickname && errors.nickname.message}</ErrorText>
-            <Controller
-              control={control}
-              defaultValue=""
-              name="email"
-              rules={{
-                required: "필수 입력사항입니다.",
-                pattern: {
-                  value: /^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/,
-                  message: "올바른 이메일 형식인지 다시 확인해주세요.",
-                },
-              }}
-              render={({ field }) => (
+              </InputWrapper>
+            )}
+          />
+          <ErrorText>{errors.nickname && errors.nickname.message}</ErrorText>
+          <Controller
+            control={control}
+            defaultValue=""
+            name="email"
+            rules={{
+              required: "필수 입력사항입니다.",
+              pattern: {
+                value: /^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/,
+                message: "올바른 이메일 형식인지 다시 확인해주세요.",
+              },
+            }}
+            render={({ field }) => (
+              <InputWrapper>
+                <Label>이메일</Label>
                 <Input
-                  label="이메일"
                   type="email"
                   placeholder="이메일에 오타가 없는지 확인해주세요."
                   value={field.value}
                   onChange={field.onChange}
                   autoComplete="off"
+                  width="20vw"
+                  height="4vh"
                 />
-              )}
-            />
-            <ErrorText>{errors.email && errors.email.message}</ErrorText>
-            <Controller
-              control={control}
-              defaultValue=""
-              name="password"
-              rules={{
-                required: "필수 입력사항입니다.",
-                pattern: {
-                  value: /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{8,15}$/,
-                  message:
-                    "8-15자 or 최소 1개의 숫자 혹은 특수문자를 포함했는지 확인해주세요.",
-                },
-              }}
-              render={({ field }) => (
+              </InputWrapper>
+            )}
+          />
+          <ErrorText>{errors.email && errors.email.message}</ErrorText>
+          <Controller
+            control={control}
+            defaultValue=""
+            name="password"
+            rules={{
+              required: "필수 입력사항입니다.",
+              pattern: {
+                value: /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{8,15}$/,
+                message:
+                  "8-15자 or 최소 1개의 숫자 혹은 특수문자를 포함했는지 확인해주세요.",
+              },
+            }}
+            render={({ field }) => (
+              <InputWrapper>
+                <Label>비밀번호</Label>
                 <Input
-                  label="비밀번호"
                   type="password"
                   placeholder="8-15자 대/소문자 + 최소 1개의 숫자 혹은 특수문자 포함"
                   value={field.value}
                   onChange={field.onChange}
                   autoComplete="off"
+                  width="20vw"
+                  height="4vh"
                 />
-              )}
-            />
-            <ErrorText>{errors.password && errors.password.message}</ErrorText>
-            <Controller
-              control={control}
-              defaultValue=""
-              name="passwordCheck"
-              rules={{
-                required: "필수 입력사항입니다.",
-              }}
-              render={({ field }) => (
+              </InputWrapper>
+            )}
+          />
+          <ErrorText>{errors.password && errors.password.message}</ErrorText>
+          <Controller
+            control={control}
+            defaultValue=""
+            name="passwordCheck"
+            rules={{
+              required: "필수 입력사항입니다.",
+            }}
+            render={({ field }) => (
+              <InputWrapper>
+                <Label>비밀번호 확인</Label>
                 <Input
-                  label="비밀번호 확인"
                   type="password"
                   placeholder="비밀번호를 한번 더 입력해주세요."
                   value={field.value}
                   onChange={field.onChange}
                   autoComplete="off"
+                  width="20vw"
+                  height="4vh"
                 />
-              )}
-            />
-            <ErrorText>
-              {errors.passwordCheck && errors.passwordCheck.message}
-            </ErrorText>
-            <Button type="submit" backgroundColor="#f2e678">
-              Sign up
-            </Button>
-          </form>
-        </SingUpWrapper>
-      </Card>
-    </MainWrapper>
+              </InputWrapper>
+            )}
+          />
+          <ErrorText>
+            {errors.passwordCheck && errors.passwordCheck.message}
+          </ErrorText>
+          <Button type="submit" backgroundColor="#f2e678" margin="1.5rem">
+            Sign up
+          </Button>
+        </form>
+      </SingUpWrapper>
+    </Card>
   );
 };
 
