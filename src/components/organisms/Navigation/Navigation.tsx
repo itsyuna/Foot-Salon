e8152a4df6c96118f66475ff8fa5416011ffe3a6
itsyuna <ingyouna@naver.com>
2023-11-30 20:46:58 +0900

    Responsive web: NavBar

--- src/components/organisms/Navigation/Navigation.tsx
+++ src/components/organisms/Navigation/Navigation.tsx
@@ -1,25 +1,99 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { HiMenu } from "react-icons/hi";
+import { MdClose } from "react-icons/md";
 
-const NavWrapper = styled.nav`
+import { media } from "../../../ui/MediaQuery/mediaQuery";
+
+const NavWrapper = styled.nav<{ extendNavBar: boolean }>`
   background-color: #33aa30;
   border-radius: 20px;
   width: 100%;
-  height: 5%;
+  height: 45px;
   line-height: 50%;
   position: relative;
-  margin-bottom: 10px;
+  margin: 10px 0;
+
+  div:last-child {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  svg {
+    opacity: 0;
+  }
+
+  @media screen and (max-width: 767px) {
+    svg {
+      z-index: 999;
+    }
+    svg:hover {
+      cursor: pointer;
+    }
+
+    ${({ extendNavBar }) =>
+      extendNavBar
+        ? css`
+            border-radius: 0;
+            background-color: #79ac78;
+
+            ul {
+              background-color: #79ac78;
+            }
+
+            div:last-child svg:nth-child(2) {
+              opacity: 1;
+              margin-right: 5%;
+            }
+          `
+        : css`
+            ul {
+              transform: translateX(100vh);
+            }
+
+            div:last-child svg:nth-child(1) {
+              opacity: 1;
+              margin-left: 5%;
+            }
+          `}
+
+    ul {
+      position: fixed;
+      top: 18%;
+      left: 10%;
+      width: 80vw;
+      height: 81vh;
+      margin: 0;
+      z-index: 999;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: top;
+    }
+
+    height: 30px;
+  }
 `;
 
 const IconBox = styled.div`
   height: 100%;
+
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  ${media.small`
+    width: 25%;
+    display: none;
+  `}
 `;
 
 const GoalpostIcon = styled.img`
-  margin-left: 40px;
+  width: 8vw;
+  margin-left: 3%;
 `;
 
 const SonnyCelebrationIcon = styled.img`
@@ -29,20 +103,31 @@ const SonnyCelebrationIcon = styled.img`
 
 const Ul = styled.ul`
   position: absolute;
-  width: 95%;
-  top: 10%;
+
+  width: 100%;
+  justify-content: space-around;
+
+  top: 0;
 `;
 
-const HomeMenu = styled(Link)`
+const HomeMenu = styled(NavLink)`
   font-family: "Orelega One", cursive;
-  font-size: 2rem;
+  font-size: 1.5rem;
   color: black;
-  margin-left: 95px;
+
+  ${media.small`
+    font-size: 1.5rem;
+  `}
 `;
 
-const Menu = styled(HomeMenu)`
+const Menu = styled(NavLink)`
+  display: grid;
+  grid-template-columns: repeat(10%, auto);
   font-family: "Swanky and Moo Moo", cursive;
-  font-size: 2.5rem;
+  height: 100%;
+
+  font-size: 1.8rem;
+
   text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
   color: white;
 `;
@@ -50,9 +135,19 @@ const Menu = styled(HomeMenu)`
 const GoalPost = `${process.env.PUBLIC_URL}/assets/icons/goalpost.svg`;
 const SonnyCelebration = `${process.env.PUBLIC_URL}/assets/icons/sonny-celebration.svg`;
 
+const MenuList = [
+  ["/line-up", "Line-Up"],
+  ["/play", "Play"],
+  ["/half-time", "Half-Time"],
+  ["/stats", "Stats"],
+  ["/photos", "Photos"],
+];
+
 const Navigation = () => {
+  const [extendNavBar, setExtendNavBar] = useState(false);
+
   return (
-    <NavWrapper>
+    <NavWrapper extendNavBar={extendNavBar}>
       <IconBox>
         <GoalpostIcon src={GoalPost} alt="Goalpost icon" />
         <SonnyCelebrationIcon
@@ -60,26 +155,29 @@ const Navigation = () => {
           alt="Sonny's celebration icon"
         />
       </IconBox>
+
       <Ul>
-        <li>
+        <li onClick={() => setExtendNavBar(!extendNavBar)}>
           <HomeMenu to="/">Home</HomeMenu>
         </li>
-        <li>
-          <Menu to="/line-up">Line-Up</Menu>
-        </li>
-        <li>
-          <Menu to="/play">Play</Menu>
-        </li>
-        <li>
-          <Menu to="/half-time">Half-Time</Menu>
-        </li>
-        <li>
-          <Menu to="/stats">Stats</Menu>
-        </li>
-        <li>
-          <Menu to="/photos">Photos</Menu>
-        </li>
+        {MenuList.map((list, idx) => (
+          <li key={idx} onClick={() => setExtendNavBar(!extendNavBar)}>
+            <Menu to={list[0]}>{list[1]}</Menu>
+          </li>
+        ))}
       </Ul>
+      <div>
+        <HiMenu
+          size="30"
+          color="white"
+          onClick={() => setExtendNavBar(!extendNavBar)}
+        />
+        <MdClose
+          size="30"
+          color="white"
+          onClick={() => setExtendNavBar(!extendNavBar)}
+        />
+      </div>
     </NavWrapper>
   );
 };
